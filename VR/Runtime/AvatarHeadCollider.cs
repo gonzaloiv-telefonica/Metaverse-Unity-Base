d77968d81a2b7bafbd8c452506671624911a756b
gonzaloiv <gonzalo.iglesiasvilachan@telefonica.com>
2022-10-19 08:45:38 +0200

    Folder reorganization

--- VR/Runtime/AvatarHeadCollider.cs
+++ VR/Runtime/AvatarHeadCollider.cs
@@ -2,16 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 namespace Meta.VR
 {
     public class AvatarHeadCollider : MonoBehaviour
     {
         [SerializeField] Transform centerEyeAnchor;
-        
+
         void Update()
         {
             transform.position = centerEyeAnchor.position;
         }
-}
+    }
 }
